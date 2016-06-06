e6a25066bf9ce1229fb6a0d10ecda9d99a4a9349
Mark Tyneway <tyneway12@gmail.com>
2016-06-06 15:01:06 -0700

    Add app.jsx scaffolding

---
+++ client/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Components/navbar/navbar.jsx';
+import Http from './Components/http/http.jsx';
+
+const app = () => {
+  <div>
+    <Navbar />
+    <Http />
+  <div>
+}
+
+export default app;
