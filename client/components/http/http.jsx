f4f55f2c2f522e3b7d1654d37f37c9d91a055de1
Mark Tyneway <tyneway12@gmail.com>
2016-06-06 15:09:56 -0700

    Add navbar and http scaffolding

---
+++ client/components/http/http.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const http = () => {
+  <h1>Http here</h1>
+}
+
+export default http;
---
+++ client/components/navbar/navbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const navbar = () => {
+  <h1>Navbar here</h1>
+}
+
+export default navbar;
