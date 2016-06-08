70cad6933cff4af8230ceb11c7df0e9135eb22e6
Mark Tyneway <tyneway12@gmail.com>
2016-06-08 12:14:13 -0700

    Add tests for App component

--- client/app.test.jsx
+++ client/app.test.jsx
@@ -4,10 +4,21 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import App from './app.jsx';
+import Navbar from './Components/navbar/navbar.jsx';
+import Http from './Components/http/http.jsx';
 
 describe('App Component', function () {
+  let wrapper;
+  before(function () {
+    wrapper = shallow(<App />);
+  });
   it('Renders without a problem', function () {
-    const wrapper = shallow(<App />);
     expect(wrapper.is('div')).to.equal(true);
+  });
+  it('Contains the Navbar and Http Components', function () {
+    expect(wrapper.contains([
+      <Navbar />,
+      <Http />,
+    ]));
   });
 });
