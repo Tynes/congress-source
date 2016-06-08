d4bbb088d88ad32ee0c5f9471b4afa7ec6b47f55
Mark Tyneway <tyneway12@gmail.com>
2016-06-08 12:00:06 -0700

    Implement single working test and refactor using linter

--- client/app.test.jsx
+++ client/app.test.jsx
@@ -2,15 +2,12 @@
 /* eslint prefer-arrow-callback: "off", func-names: "off" */
 import React from 'react';
 import { expect } from 'chai';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from './app.jsx';
 
-describe('The app', function () {
-  it('contains spec with an expectation', function () {
-    expect(shallow(<App />).is('.app')).to.equal(true);
-  });
-
-  it('contains spec with an expectation', function () {
-    expect(mount(<App />).find('.app').length).to.equal(1);
+describe('App Component', function () {
+  it('Renders without a problem', function () {
+    const wrapper = shallow(<App />);
+    expect(wrapper.is('div')).to.equal(true);
   });
 });
