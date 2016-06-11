0aa4d5e3cbaa8f12a99594428fd84d85b520fd03
Mark Tyneway <tyneway12@gmail.com>
2016-06-11 10:26:38 -0700

    Add textfield and radiobuttons

--- client/components/search/search.jsx
+++ client/components/search/search.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
+import TextField from 'material-ui/TextField';
 
 class Search extends React.Component {
   render() {
     return (
-      <div>Search</div>
+      <div>
+        <TextField
+          floatingLabelText="Search Here"
+          hintText="Ex Chuck Schumer or New York"
+          floatingLabelFixed={true}
+        />
+        <RadioButtonGroup name="searchType" defaultSelected="state">
+          <RadioButton
+            value="state"
+            label="State"
+          />
+          <RadioButton
+            value="name"
+            label="Name"
+          />
+        </RadioButtonGroup>
+      </div>
     );
   }
 }
