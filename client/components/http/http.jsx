8890fc8513e1ca34ef5c87027ef6971ed9d45fd1
Mark Tyneway <tyneway12@gmail.com>
2016-06-11 12:19:40 -0700

    Set up http request to json-server

--- client/components/http/http.jsx
+++ client/components/http/http.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import Results from '../results/results.jsx';
+import axios from 'axios';
 
 class Http extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      people: [],
+    };
+  }
+  componentDidMount() {
+    this.getPeople();
+  }
+  getPeople() {
+    axios.get('http://localhost:3000/objects?_start=0&_end=10')
+      .then(response => {
+        console.log(response);
+        this.setState({ people: response.data });
+      });
+  }
   render() {
     return (
-      <Results />
+      <Results people={this.state.people} />
     );
   }
 }
