7dd652528a3ba68cfe29470f965e0daa22b87726
Mark Tyneway <tyneway12@gmail.com>
2016-06-11 16:24:06 -0700

    Pass down simplified data to components

---
+++ client/components/result/picture/picture.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Picture = ({ id }) => {
+  return (
+    <h1>{id}</h1>
+  );
+};
+
+export default Picture;
--- client/components/result/result.jsx
+++ client/components/result/result.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import Name from './name/name.jsx';
+import Info from './info/info.jsx';
+import Picture from './picture/picture.jsx';
+import State from './state/state.jsx';
+import Timer from './timer/timer.jsx';
 
 class Result extends React.Component {
+  componentWillMount() {
+    console.log(this.props);
+  }
   render() {
     return (
-      <h1>{this.props.member.person.firstname}</h1>
+      <div>
+        <Picture id={this.props.member.id} />
+      </div>
     );
   }
 }
 
 export default Result;
+
+/*
+
+<Name name={this.props} />
+<Info info={true} />
+<State state={this.props.state} />
+<Timer timer={true} />
+*/
