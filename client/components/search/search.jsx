c10b0fa2fadf9dae9261a98a6e047e75eda62009
Mark Tyneway <tyneway12@gmail.com>
2016-06-11 09:50:00 -0700

    Initialize search component

---
+++ client/components/search/search.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div>Search</div>
+    );
+  }
+}
+
+export default Search;
+
+/*
+2 options to search by
+state and name
+
+*/
