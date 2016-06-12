4941b7db5f05114ba7a257d155450d3b7fb8bf0a
Mark Tyneway <tyneway12@gmail.com>
2016-06-11 17:16:44 -0700

    Test for presence of rendered app

--- client/app.test.jsx
+++ client/app.test.jsx
@@ -1,11 +1,15 @@
 /* eslint-env mocha */
 /* eslint prefer-arrow-callback: "off", func-names: "off" */
 import React from 'react';
-import { expect } from 'chai';
+import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme';
 import { shallow } from 'enzyme';
 import App from './app.jsx';
 import Navbar from './Components/navbar/navbar.jsx';
 import Http from './Components/http/http.jsx';
+
+const expect = chai.expect;
+chai.use(chaiEnzyme()); // Note the invocation at the end
 
 describe('App Component', function () {
   let wrapper;
@@ -13,7 +17,7 @@ describe('App Component', function () {
     wrapper = shallow(<App />);
   });
   it('Renders without a problem', function () {
-    expect(wrapper.is('div')).to.equal(true);
+    expect(wrapper).to.be.present();
   });
   it('Contains the Navbar and Http Components', function () {
     expect(wrapper.contains([
