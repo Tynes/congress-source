e8eaadd963707580ac3e0f8387480c86b991a0c8
Mark Tyneway <tyneway12@gmail.com>
2016-06-15 14:50:45 -0700

    Increase size of search bar

--- client/components/search/search.jsx
+++ client/components/search/search.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
 import TextField from 'material-ui/TextField';
 
+const style = {
+  width: '376px',
+};
+
+// style sets the width of the form
+// need to figure out how to make it responsive
+
 class Search extends React.Component {
   render() {
     return (
@@ -13,6 +20,7 @@ class Search extends React.Component {
             hintText="Ex Chuck Schumer or New York"
             floatingLabelFixed={true}
             className="row"
+            style={style}
           />
           <RadioButtonGroup name="searchType" defaultSelected="state" className="row">
             <RadioButton
