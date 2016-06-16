18c349c80a606e0a9ca521fbc16fe3cde1ca85b7
Mark Tyneway <tyneway12@gmail.com>
2016-06-16 15:11:22 -0700

    Add mongodb/mongoose connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const PORT = process.env.port || 3001;
+
 const express = require('express');
 const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.static(`${__dirname}/../client`));
 app.use(bodyparser.json());
+
+if (process.env.ENV === 'dev') {
+  mongoose.connect('mongodb://localhost/congress');
+  console.log('connected to mongoose');
+} else {
+  console.log('set the env var to dev for development');
+}
 
 app.listen(PORT, () => {
   console.log(`listening on port:${PORT}`);
