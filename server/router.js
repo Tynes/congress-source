0e21c6b34037e93fbb219ae6223673ed1c4a46ee
Mark Tyneway <tyneway12@gmail.com>
2016-06-19 14:32:47 -0700

    Set up route for querying members between 2 queryIDs

--- server/router.js
+++ server/router.js
@@ -1,9 +1,16 @@
 const memberCtrl = require('./controllers/memberController.js');
 
 module.exports = app => {
-  app.get('/members', (req, res) => {
+  app.get('/allMembers', (req, res) => {
     memberCtrl.getMembers()
       .then(members => res.send(members))
       .catch(err => console.log(err));
+  });
+  app.get('/members', (req, res) => {
+    const begin = req.query.begin;
+    const end = req.query.end;
+    memberCtrl.getMembersBetween(begin, end)
+      .then(members => res.send(members))
+      .catch(err => console.log('error in route', err));
   });
 };
