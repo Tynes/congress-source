9a25d74144abe8f9eb4c4105690b614db8d18e8f
Mark Tyneway <tyneway12@gmail.com>
2016-06-18 18:18:03 -0700

    Set up db seed with environmental variable

--- server/server.js
+++ server/server.js
@@ -4,22 +4,23 @@ const bodyparser = require('body-parser');
 const PORT = process.env.port || 3001;
 
 const app = express();
+const db = require('./db.js');
 app.use(express.static(`${__dirname}/../client`));
 app.use(bodyparser.json());
 require('./router.js')(app);
 
 // to seed db initially
 // TODO: Add this functionality in a cron job
-// const memberCtrl = require('./controllers/memberController.js');
-// db.once('open', () => {
-//   console.log('open');
-//   // initial population of db
-//   const ff = memberCtrl.getRawMembers();
-
-//   ff.then(data => {
-//     memberCtrl.updateAll(data);
-//   });
-// });
+const memberCtrl = require('./controllers/memberController.js');
+db.once('open', () => {
+  console.log('open');
+  // initial population of db
+  if (process.env.SEED) {
+    console.log('seeding db');
+    memberCtrl.getRawMembers()
+      .then(data => memberCtrl.syncDB(data));
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port:${PORT}`);
