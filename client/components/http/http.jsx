6fef99f5305ea43f3573e60c2f30651d5235d1a4
Mark Tyneway <tyneway12@gmail.com>
2016-06-19 14:36:47 -0700

    Set up GET request to query between a range of members

--- client/components/http/http.jsx
+++ client/components/http/http.jsx
@@ -7,20 +7,38 @@ class Http extends React.Component {
     super();
     this.state = {
       members: [],
+      end: 0,
     };
   }
   componentWillMount() {
-    this.getMembers();
+    // this.getMembers();
+    this.getMembersBetween(0, 16);
   }
   getMembers() {
     // query for all members
-    axios.get('/members')
-      .then(response => this.setState({ members: response.data }));
+    axios.get('/allMembers')
+      .then(response => this.setState({ members: response.data }))
+      .catch(err => console.log('error getting all members', err));
+  }
+  getMembersBetween(begin, end) {
+    axios.get(`/members?begin=${begin}&end=${end}`)
+      .then(response => {
+        const state = {
+          members: this.state.members.concat(response.data),
+          end: this.state.end + (begin - end),
+        };
+        this.setState(state);
+      })
+      .catch(err => console.log('error getting members between', err));
   }
 
   render() {
     return (
-      <Results members={this.state.members} />
+      <Results
+        members={this.state.members}
+        getMembersBetween={this.getMembersBetween}
+        end={this.state.end}
+      />
     );
   }
 }
