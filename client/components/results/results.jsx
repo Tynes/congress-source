754942c691002d2cb2bad85f9dc4a2aeecb821a8
Mark Tyneway <tyneway12@gmail.com>
2016-06-19 14:35:19 -0700

    Render only 16 members and begin to set up infinite scroll

--- client/components/results/results.jsx
+++ client/components/results/results.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import _ from 'underscore';
 import Result from '../result/result.jsx';
+import Infinite from 'react-infinite';
 
 class Results extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
       <div className="members">
@@ -18,3 +20,12 @@ class Results extends React.Component {
 }
 
 export default Results;
+
+// <Infinite
+  // elementHeight={320}
+  // containerHeight={320 * 4}
+  // infiniteLoadBeginEdgeOffset={200}
+  // onInfiniteLoad={this.props.getMembersBetween(this.props.end - 16, this.props.end)}
+// >
+  // {this.props.members}
+// </Infinite>
