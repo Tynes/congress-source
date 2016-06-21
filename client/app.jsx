1aa0b07f66e4e5d71d6ea43aba0f32779082b972
Mark Tyneway <tyneway12@gmail.com>
2016-06-21 14:34:34 -0700

    Clean up old comments

--- client/app.jsx
+++ client/app.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Navbar from './Components/navbar/navbar.jsx';
-// import Search from './Components/search/search.jsx';
 import Http from './Components/http/http.jsx';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
