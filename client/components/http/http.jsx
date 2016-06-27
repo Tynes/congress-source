6d0f5c4354c637c8051d4c3bdab9e85dcf1d52ce
Mark Tyneway <tyneway12@gmail.com>
2016-06-27 15:22:33 -0700

    Refactor into single search function and clean up code

--- client/components/http/http.jsx
+++ client/components/http/http.jsx
@@ -8,38 +8,15 @@ class Http extends React.Component {
     super();
     this.state = {
       members: [],
+      begin: 0,
       end: 8,
       party: undefined,
       searchBy: 'state',
     };
   }
   componentWillMount() {
-    // initial population of members
-    this.getMembersBetween(0, 8, 8);
+    this.search(undefined, 0, {});
   }
-  getMembers() {
-    // query for all members
-    axios.get('/allMembers')
-      .then(response => this.setState({ members: response.data }))
-      .catch(err => console.log('error getting all members', err));
-  }
-
-  getPartyMembersBetween(party, begin, end, shift) {
-    axios.get(`/party?party=${party}&begin=${begin}&end=${end}`)
-      .then(response => {
-        console.log('shift', shift);
-        const state = {
-          members: response.data,
-          end: this.state.end + shift,
-          party: party,
-          searchBy: this.state.searchBy,
-        };
-        console.log('state from party', state);
-        this.setState(state);
-      })
-      .catch(err => console.log('error get party', err));
-  }
-
   handleCheck(party) {
     if (this.state.party === party) {
       this.setState({ party: undefined }, () => this.getMembersBetween(0, 8, 8));
@@ -47,74 +24,40 @@ class Http extends React.Component {
       this.getPartyMembersBetween(party, 0, 8, 8);
     }
   }
-
-  getMembersBetween(begin, end, shift) {
-    axios.get(`/members?begin=${begin}&end=${end}`)
-      .then(response => {
-        const state = {
-          members: response.data,
-          end: this.state.end + shift,
-        };
-        this.setState(state);
-      })
-      .catch(err => console.log('error getting members between', err));
-  }
-  // will display 8 members per page
-  getNextMembers() {
-    if (this.state.party) {
-      this.getPartyMembersBetween(this.state.party, this.state.end - 8, this.state.end, 8);
-    } else {
-      this.getMembersBetween(this.state.end - 8, this.state.end, 8);
-    }
-  }
-  getPrevMembers() {
-    if (this.state.party) {
-      this.getPartyMembersBetween(this.state.party, this.state.end - 24, this.state.end - 16, -8);
-    }
-    this.getMembersBetween(this.state.end - 24, this.state.end - 16, -8);
-  }
-  search(query) {
-    axios.get(`/search?name=${query}`)
-      .then(response => {
-        const state = {
-          members: response.data,
-          end: 8,
-          party: undefined,
-          searchBy: this.state.searchBy,
-        };
-        this.setState(state);
-      })
-      .catch(err => console.log('error in search', err));
-  }
-  searchByState(query) {
-    axios.get(`/state?state=${query}`)
-      .then(response => {
-        const state = {
-          members: response.data,
-          end: 8,
-          party: undefined,
-          searchBy: this.state.searchBy,
-        };
-        this.setState(state);
-      })
-      .catch(err => console.log('error in searchByState', err));
-  }
   handleSearchToggle(parameter) {
     this.setState({ searchBy: parameter });
   }
-  handleSearch(query) {
-    if (this.state.searchBy === 'name') {
-      if (query.length > 2) {
-        this.search(query);
-      } else if (query.length <= 1) {
-        this.getMembersBetween(0, 8, 8);
-      }
-    } else if (this.state.searchBy === 'state') {
-      if (query.length > 1) {
-        this.searchByState(query);
-      } else if (query.length <= 1) {
-        this.getMembersBetween(0, 8, 8);
-      }
+  search(query, shift, options) {
+    const params = Object.keys(options).map(el => `${el}=${options[el]}&query=${query}`).join('');
+    axios.get(`/search?${params}`)
+      .then(response => {
+        console.log(response);
+        const state = {
+          members: response.data,
+          begin: this.state.begin + shift,
+          end: this.state.end + shift,
+          party: options.party,
+          searchBy: this.state.searchBy,
+        };
+        this.setState(state, () => console.log(state));
+      })
+      .catch(err => console.log('error in search', err));
+  }
+  handleSearch(query, shift) {
+    const options = {
+      type: this.state.searchBy,
+      begin: this.state.begin,
+      end: this.state.end,
+    };
+    if (this.state.party) options.party = this.state.party;
+    if (this.state.searchBy === 'name' && query.length > 2) {
+      this.search(query, shift, options);
+    } else if (this.state.searchBy === 'state' && query.length > 1) {
+      // TODO: abstract search more
+      // send undefined as query to get initial members
+      this.search(query, shift, options);
+    } else {
+      this.getMembersBetween(0, 0, 0);
     }
   }
 
@@ -123,8 +66,6 @@ class Http extends React.Component {
       <div>
         <Search
           handleSearch={this.handleSearch.bind(this)}
-          getNextMembers={this.getNextMembers.bind(this)}
-          getPrevMembers={this.getPrevMembers.bind(this)}
           handleCheck={this.handleCheck.bind(this)}
           party={this.state.party}
           handleSearchToggle={this.handleSearchToggle.bind(this)}
