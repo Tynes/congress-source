2e4b8a4de4f1387a1d88dc059982dff16f60dbc8
Mark Tyneway <tyneway12@gmail.com>
2016-06-28 22:30:12 -0700

    Remove extra repeated code

--- server/router.js
+++ server/router.js
@@ -11,39 +11,19 @@ module.exports = app => {
     let dbCall;
     if (!query && !party) {
       dbCall = searchMethods.getAllMembers;
-      // searchMethods.getAllMembers()
-      //   .then(response => searchMethods.extract(response, begin, end))
-      //   .then(response => res.send(response))
-      //   .catch(err => console.log('error in router when no query', err));
     } else {
       // control flow first for name vs state
       if (searchBy === 'name') {
         if (party) {
           dbCall = searchMethods.getAllByNameAndParty;
-          // searchMethods.getAllByNameAndParty(query, party)
-          //   .then(response => searchMethods.extract(response, begin, end))
-          //   .then(response => res.send(response))
-          //   .catch(err => console.log('error in router, getAllByName', err));
         } else {
           dbCall = searchMethods.getAllByName;
-          // searchMethods.getAllByName(query)
-          //   .then(response => searchMethods.extract(response, begin, end))
-          //   .then(response => res.send(response))
-          //   .catch(err => console.log('error in search getAllByName', err));
         }
       } else if (searchBy === 'state') {
         if (party) {
           dbCall = searchMethods.getAllByStateAndParty;
-          // searchMethods.getAllByStateAndParty(query, party)
-          //   .then(response => searchMethods.extract(response, begin, end))
-          //   .then(response => res.send(response))
-          //   .catch(err => console.log('error in search getAllByStateAndParty', err));
         } else {
           dbCall = searchMethods.getAllByState;
-          // searchMethods.getAllByState(query)
-          //   .then(response => searchMethods.extract(response, begin, end))
-          //   .then(response => res.send(response))
-          //   .catch(err => console.log('error in search get all by state', err));
         }
       } else {
         res.send(404);
