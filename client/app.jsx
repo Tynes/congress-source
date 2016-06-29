910c87e72f3e2be593b071535692ef30098b3ff4
Mark Tyneway <tyneway12@gmail.com>
2016-06-28 23:52:00 -0700

    Add state to manage menu toggle

--- client/app.jsx
+++ client/app.jsx
@@ -3,16 +3,32 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Navbar from './Components/navbar/navbar.jsx';
 import Http from './Components/http/http.jsx';
+import Menu from './Components/menu/menu.jsx';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isMenuOpen: false,
+    };
+  }
+  handleMenuToggle() {
+    console.log('toggle!');
+    this.setState({ isMenuOpen: !this.state.isMenuOpen });
+  }
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme()}>
         <div>
-          <Navbar />
+          <Navbar
+            handleMenuToggle={this.handleMenuToggle.bind(this)}
+          />
+          <Menu
+            isOpen={this.state.isMenuOpen}
+          />
           <Http />
         </div>
       </MuiThemeProvider>
