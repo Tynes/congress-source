0c16571ef0e63fe72857c4cfe9642484e63775c0
Mark Tyneway <tyneway12@gmail.com>
2016-06-29 17:51:52 -0700

    Add TODO messages

--- client/components/menu/menu.jsx
+++ client/components/menu/menu.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Drawer from 'material-ui/Drawer';
 
+// TODO: add back button to close menu
+// TODO: add link to github, twitter and linkedin
+// TODO: add philosophical tidbits about me
+
 const Menu = ({ isOpen }) => (
   <Drawer open={isOpen} />
 );
