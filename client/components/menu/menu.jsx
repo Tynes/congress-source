5d423be723249871d1fbed0150d4d574a18d23ae
Mark Tyneway <tyneway12@gmail.com>
2016-07-03 13:52:07 -0700

    Abstract buttons into About component

--- client/components/menu/menu.jsx
+++ client/components/menu/menu.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import Drawer from 'material-ui/Drawer';
-import FlatButton from 'material-ui/FlatButton';
-import FontIcon from 'material-ui/FontIcon';
+import About from './about.jsx';
 import Quotes from './quotes.jsx';
-
-// TODO: add link to github, twitter and linkedin
-// TODO: add philosophical tidbits about me
 
 class Menu extends React.Component {
   constructor() {
@@ -32,32 +28,7 @@ class Menu extends React.Component {
           quoteIndex={this.state.quoteIndex}
           chooseQuote={this.chooseQuote.bind(this)}
         />
-        <div>
-          <FlatButton
-            label="GitHub"
-            linkButton={true}
-            href="https://github.com/tynes"
-            icon={<FontIcon
-              className="fa fa-github"
-            />}
-          />
-          <FlatButton
-            label="Twitter"
-            linkButton={true}
-            href="https://twitter.com/tyneslol"
-            icon={<FontIcon
-              className="fa fa-twitter"
-            />}
-          />
-          <FlatButton
-            label="LinkedIn"
-            linkButton={true}
-            href="https://www.linkedin.com/in/marktyneway"
-            icon={<FontIcon
-              className="fa fa-linkedin-square"
-            />}
-          />
-        </div>
+        <About />
       </Drawer>
     );
   }
