68a8eea7774b37150856e78d510d603969c31df9
Mark Tyneway <tyneway12@gmail.com>
2016-07-05 15:15:47 -0700

    Add switch to timer on mouseover and switch back on mouseleave

--- client/components/result/result.jsx
+++ client/components/result/result.jsx
@@ -8,17 +8,41 @@ import Timer from './timer/timer.jsx';
 import Social from './social/social.jsx';
 
 class Result extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isCountdown: false,
+    };
+  }
   componentWillMount() {
     console.log(this.props);
+  }
+  handleCountdownChange(e) {
+    e.preventDefault();
+    this.setState({ isCountdown: !this.state.isCountdown });
+  }
+  handleTouchTap(e) {
+    e.preventDefault();
+    if (e.type === 'touchend') {
+      this.handleCountdownChange(e);
+    }
   }
   render() {
     const member = this.props.member;
     return (
-      <div className="member">
+      <div
+        className="member"
+        onMouseEnter={(e) => this.handleCountdownChange(e)}
+        onMouseLeave={(e) => this.handleCountdownChange(e)}
+        onTouchTap={e => this.handleTouchTap(e)}
+      >
         <div>
           <State state={member.state} />
           <Picture id={member.id} />
-          <Timer enddate={member.enddate} />
+          <Timer
+            enddate={member.enddate}
+            isCountdown={this.state.isCountdown}
+          />
         </div>
         <div>
           <Affiliation party={member.party} />
--- client/components/result/timer/timer.jsx
+++ client/components/result/timer/timer.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import CountdownTimer from './countdownTimer.jsx';
 
-const Timer = ({ enddate }) => {
+const formatTime = time => {
+  const units = time.split('-');
+  return Date.UTC(...units);
+};
+
+const getRemainingTime = time => formatTime(time) - Date.now();
+
+const Timer = ({ enddate, isCountdown }) => {
+  if (isCountdown) {
+    return (
+      <CountdownTimer
+        className="result-top"
+        initialTimeRemaining={getRemainingTime(enddate)}
+      />
+    );
+  }
   return <h3 className="result-top">{enddate}</h3>;
 };
 
