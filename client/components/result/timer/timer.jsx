73647aab3056d9ea42bf38d41712d31ef19eb7af
Mark Tyneway <tyneway12@gmail.com>
2016-07-05 15:51:07 -0700

    Add proper time formatting, includes days instead of just up to hours

--- client/components/result/timer/timer.jsx
+++ client/components/result/timer/timer.jsx
@@ -8,12 +8,25 @@ const formatTime = time => {
 
 const getRemainingTime = time => formatTime(time) - Date.now();
 
+const formatFunc = ms => {
+  const totalSeconds = Math.round(ms / 1000);
+  const seconds = parseInt(totalSeconds % 60);
+  const minutes = parseInt(totalSeconds / 60) % 60;
+  const hours = parseInt(totalSeconds / 3600) % 24;
+  const days = parseInt(totalSeconds / 86400);
+  return `${days} days\n
+          ${hours} hours\n
+          ${minutes} minutes\n
+          ${seconds} seconds`;
+};
+
 const Timer = ({ enddate, isCountdown }) => {
   if (isCountdown) {
     return (
       <CountdownTimer
         className="result-top"
         initialTimeRemaining={getRemainingTime(enddate)}
+        formatFunc={formatFunc}
       />
     );
   }
