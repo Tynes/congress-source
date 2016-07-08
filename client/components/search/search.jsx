5c36356f0e51596bea3d3d4d2e9cc3f0711e3a55
Mark Tyneway <tyneway12@gmail.com>
2016-07-07 21:59:42 -0700

    Add flexbox for responsive design

--- client/components/search/search.jsx
+++ client/components/search/search.jsx
@@ -5,7 +5,7 @@ import FlatButton from 'material-ui/FlatButton';
 import TextField from 'material-ui/TextField';
 import ArrowForward from 'material-ui/svg-icons/navigation/arrow-forward';
 import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
-// resetting when party filter is on and search is empty
+
 const style = {
   width: '376px',
 };
@@ -40,46 +40,46 @@ class Search extends React.Component {
   }
   render() {
     return (
-      <div>
-        <h1 className="row">Search Through Members of the US Government</h1>
-        <div className="right-push">
-          <TextField
-            onChange={this.handleChange.bind(this)}
-            floatingLabelText="Search Here"
-            hintText="Ex Chuck Schumer or New York"
-            floatingLabelFixed={true}
-            className="row"
-            style={style}
-            ref={i => this.searchbar = i}
-            value={this.state.search}
+      <div className="search">
+        <h1 className="row row-responsive">Search Through Members of the US Government</h1>
+        <TextField
+          onChange={this.handleChange.bind(this)}
+          floatingLabelText="Search Here"
+          hintText="Ex Chuck Schumer or New York"
+          floatingLabelFixed={true}
+          className="row"
+          style={style}
+          ref={i => this.searchbar = i}
+          value={this.state.search}
+        />
+        <RadioButtonGroup
+          name="searchType"
+          defaultSelected="state"
+          className="row row-responsive"
+          onChange={(e, val) => this.handleSearchByToggle(val)}
+        >
+          <RadioButton
+            value="state"
+            label="State"
           />
-          <RadioButtonGroup
-            name="searchType"
-            defaultSelected="state"
-            className="row"
-            onChange={(e, val) => this.handleSearchByToggle(val)}
-          >
-            <RadioButton
-              value="state"
-              label="State"
-            />
-            <RadioButton
-              value="name"
-              label="Name"
-            />
-          </RadioButtonGroup>
-          <div className="row">
-            <Checkbox
-              label="Republican"
-              checked={this.props.party === 'Republican'}
-              onCheck={() => this.props.handlePartyChange('Republican', this.state.search)}
-            />
-            <Checkbox
-              label="Democrat"
-              checked={this.props.party === 'Democrat'}
-              onCheck={() => this.props.handlePartyChange('Democrat', this.state.search)}
-            />
-          </div>
+          <RadioButton
+            value="name"
+            label="Name"
+          />
+        </RadioButtonGroup>
+        <div className="row row-responsive">
+          <Checkbox
+            label="Republican"
+            checked={this.props.party === 'Republican'}
+            onCheck={() => this.props.handlePartyChange('Republican', this.state.search)}
+          />
+          <Checkbox
+            label="Democrat"
+            checked={this.props.party === 'Democrat'}
+            onCheck={() => this.props.handlePartyChange('Democrat', this.state.search)}
+          />
+        </div>
+        <div className="row row-responsive">
           <FlatButton
             icon={<ArrowBack />}
             onClick={() => this.props.paginate(this.state.search, -(PAGINATE_BY))}
