99dd14b23366e668022b8e5707b591a73fec7514
Mark Tyneway <tyneway12@gmail.com>
2016-07-11 22:45:44 -0700

    Add placeholders through function calls

--- client/components/results/results.jsx
+++ client/components/results/results.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import _ from 'underscore';
 import Result from '../result/result.jsx';
+import Placeholder from '../result/placeholder.jsx';
 
 class Results extends React.Component {
   constructor(props) {
     super(props);
   }
+  addPlaceholders(currentSize, targetSize) {
+    console.log('add', targetSize - currentSize);
+    return _.times(targetSize - currentSize, () => <Placeholder />);
+  }
+
+  buildCards(members) {
+    return _.map(members, member =>
+      <Result key={member.queryID} member={member} />
+    ).concat(this.addPlaceholders(members.length, 8));
+  }
 
   render() {
     return (
       <div className="members">
-        {_.map(this.props.members, member =>
-          <Result key={member.queryID} member={member} />
-        )}
+        {this.buildCards(this.props.members)}
       </div>
     );
   }
